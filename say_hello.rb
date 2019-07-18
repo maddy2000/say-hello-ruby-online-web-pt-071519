b75e4a6f0d19c0a6156e9e636c0b8d2a01dc75ac
Madelyn Deshield <madelynwallop@gmail.com>
2019-07-18 23:07:18 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
 # Build your say_hello method here
+
+def greeting say_hello ("Gabriela")
+  puts "hello Gabriela! do"
+end  
+ 
+ def greeting(name="ruby programmer")
+   puts "hello, #{name} do"
+ end
